49319329d8348cb0dc8b6c3daf5cdb3490876229
김가원(Kimgawon) <202021000@sangmyung.kr>
2025-01-15 18:29:45 +0900

    [#1] - 백엔드 프로젝트 세팅

---
+++ back-end/src/main/java/demo/back_end/SocialLoginApplication.java
@@ -0,0 +1,13 @@
+package demo.back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialLoginApplication.class, args);
+	}
+
+}
